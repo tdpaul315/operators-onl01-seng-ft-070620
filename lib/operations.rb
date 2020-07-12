9ce93b064258e2ffb5e08996c0cd01b70191321b
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-12 00:22:28 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
-def unsafe?(speed)
-
-end
-
-
-
 def not_safe?(speed)
-	
+
+speed < 40 || speed > 60 ? true : false
+
 end
-	
+
+def unsafe?(speed)
+  if speed < 40 || speed > 60
+    return true
+  elsif speed > 40 || speed < 60
+  return false
+  end 
+end
+
+
+
+
+
+
 
 
